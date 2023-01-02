32f64bb38f334dd81c700f87e6b3ccce56a38cfb
Scotty McCarthy <119365979+scottymccarthy@users.noreply.github.com>
2023-01-01 22:12:34 -0700

    Update caesar.js

--- Project-_Decoder_ring-Scotty_mccarthy-Solution/src/caesar.js
+++ Project-_Decoder_ring-Scotty_mccarthy-Solution/src/caesar.js
@@ -4,11 +4,44 @@
 // of the anonymous function on line 6
 
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-  function caesar(input, shift, encode = true) {
-    // your solution code here
-  }
+   function caesar(input, shift, encode = true){
+//If the shift value isn't present, equal to 0, less than -25, or greater than 25, the function should return false
+if (!shift || shift >= alphabet.length || shift <=(alphabet.length)*-1) {return false;}
+//if decode, inverts negative number back to positive
+if (encode === false){ 
+      shift = shift * (-1); 
+    };
+//Capital letters can be ignored
+input = input.toLowerCase();
+let newInput = ""; 
+
+  for (let i = 0; i < input.length; i++){
+        const letter = input[i]; 
+        if (!alphabet.includes(letter)){
+           newInput += letter;
+          } 
+  
+  for (let i = 0; i < alphabet.length; i++){
+        if (letter == alphabet[i]) {
+
+ //End of alphabet wraps back to beginning         
+              if(i + shift >= 26){
+   newInput += alphabet[(i+shift) - 26];
+              } 
+//beginning of alphabet wraps around to the end               
+          else if(i + shift < 0) {                
+                  newInput += alphabet[26 + (i+shift)]
+                } 
+          else {
+                  newInput += alphabet[i+shift];
+            }
+          }
+        }
+      } 
+    return newInput;
+}
 
   return {
     caesar,
